64418a9851fd3e4b6d3b16d8bcc6e9a5a885f4c5
Brian Blosser <bpb54321@gmail.com>
2018-03-17 21:52:45 -0400

    Finished the test of the sample schedule scrolling link.

--- webdriver/academics-test-2.js
+++ webdriver/academics-test-2.js
@@ -27,15 +27,7 @@ const util = require('./utilities');
   builder = new webdriver.Builder().forBrowser('chrome');
   driver = await builder.build();
 
-  // Login to WP Admin to enable viewing of the private page
-  await driver.get('http://learningdesign.georgetown.domains/mld-staging/academics/');
-
-  let passwordInput = await driver.wait(until.elementLocated(By.id('password')), 10000);
-
-  await passwordInput.sendKeys('scholarship!')
-  
-  // Submit the form that contains passwordInput
-  await passwordInput.submit();
+  await driver.get('http://learninganddesign.georgetown.local/academics/');
 
   // Wait for the actual Academics page to load
   heading_1 = await driver.wait(until.elementLocated(By.css('h1.feature__heading')), 10000, 
@@ -110,21 +102,45 @@ async function testBookmarks(sampleStudentScheduleLink, driver) {
   let block_name = 'sample schedule link';
   let window_start_scroll_position;
   let window_end_scroll_position;
+
+  // The vertical scroll position of this element relative to the document.
+  let sample_schedule_scroll_position;
+
+  // We want the window to scroll to the target, plus this amount.
+  // Topbar = 67px;
+  // Extra space = 50px;
+  let scroll_padding = -(67 + 50);
+
+  // The target scroll position + scroll_padding
+  let expected_window_scroll_position;
   
   console.log(colors.yellow(`Testing ${block_name}\n`));
   
   try {
 
     // Get the start scroll position of the window
+    // 0
     window_start_scroll_position = await driver.executeScript(function() {
       return jQuery(window).scrollTop();
     });
+
+    // Get the scroll position of #sample-schedule relative to the document
+    // 4374
+    sample_schedule_scroll_position = await driver.executeScript(function() {
+      var $sample_schedule = jQuery('#sample-schedule');
+      var offset_object = $sample_schedule.offset();
+      var offset_top = offset_object.top;
+      return offset_top;
+    });
+
+    // 4374 - 117 = 4257
+    expected_window_scroll_position = sample_schedule_scroll_position + scroll_padding;
 
     // Click on the link
     await sampleStudentScheduleLink.click();
 
     // Wait the transition time for the smooth scroll
-    driver.sleep(1500);
+    await driver.sleep(1500);
 
     window_end_scroll_position = await driver.executeScript(function() {
       return jQuery(window).scrollTop();
@@ -133,13 +149,17 @@ async function testBookmarks(sampleStudentScheduleLink, driver) {
     expect(window_start_scroll_position).to.not.equal(window_end_scroll_position);
     console.log(colors.green(`\tVerified that the window scroll position changes after you click the link.\n`));
 
+    expect(window_end_scroll_position).to.equal(expected_window_scroll_position);
+    console.log(colors.green(`\tVerified that the window scrolls to the same position (plus padding) of ${block_name}\n`));
+
   } catch(error) {
-    printErrorAndExit(error);
+    await printErrorAndExit(error, driver);
   }
 
 }
 
-function printErrorAndExit(error) {
+async function printErrorAndExit(error, driver) {
   console.log(colors.red(error.stack));
+  await driver.quit();
   process.exit(1);
 }
